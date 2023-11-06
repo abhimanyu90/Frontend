a06be46cf1c4f94689ea150930330d659f962f5d
abhimanyu90 <96384184+abhimanyu90@users.noreply.github.com>
2023-11-06 11:56:43 +0530

    Create ExampleComponent.js

---
+++ src/ExampleComponent.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import API_BASE_URL from './config';
+
+const ExampleComponent = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/endpoint`);
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+        } else {
+          throw new Error('Failed to fetch data');
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!data) {
+    return <div>No data found.</div>;
+  }
+
+  // Render the fetched data
+  return (
+    <div>
+      <h1>Fetched Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
